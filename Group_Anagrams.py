6530be478dbcd3b22813305083f24fed229fb867
Ahnaf-Sayem <ahnafsayeem.ctg@gmail.com>
2026-08-08 02:19:22 -0700

    Compact solution

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -1,12 +1,20 @@
-from collections import defaultdict
+import string
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        grp_of_anagrams=defaultdict(list)
+    def Group_of_Anagrams(self,strs):
+        i = 0
+        alphabet_order = {}
+        for lower_case_alphabet in string.ascii_lowercase:
+            alphabet_order[lower_case_alphabet] = i
+            i += 1
+        final_list = {}
         for word in strs:
-            count=[0]*26
+            alphabet_structure = [0] * 26
             for letter in word:
-                count[ord(letter)-ord('a')]+=1
-            grp_of_anagrams[tuple(count)].append(word)
-        return list(grp_of_anagrams.values())
-
-
+                alphabet_structure [alphabet_order[letter]] += 1
+            alphabet_structure =  tuple(alphabet_structure)
+            if alphabet_structure in final_list:
+                final_list[alphabet_structure].append(word)
+            elif alphabet_structure not in final_list:
+                final_list[alphabet_structure] = []
+                final_list[alphabet_structure].append(word)
+        return list(final_list.values())
