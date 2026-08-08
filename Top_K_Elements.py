e7468a6a3ddbc198074ec46ab35bd2a7952d16fb
Ahnaf-Sayem <ahnafsayeem.ctg@gmail.com>
2026-08-08 03:16:09 -0700

    compact solution

--- Top_K_Elements.py
+++ Top_K_Elements.py
@@ -1,22 +1,26 @@
-from collections import defaultdict
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums_count = {}
+        buckets_by_order = [set() for _ in range(len(nums))]
+        nums_count = {} # number as a key and frequency as a value
         for number in nums:
-            nums_count[number] = nums_count.get(number, 0) + 1
-
-        buckets = [set() for _ in range(len(nums))]
-        for number, freq in nums_count.items():
-            buckets[freq - 1].add(number)
-
-        non_empty_buckets = [bucket for bucket in buckets if bucket]
-
-        final_list = []
-        for bucket in reversed(non_empty_buckets):
+            nums_count[number] = nums_count.get(number,0) + 1 # starts creating the nums_count
+        for number in nums:
+            buckets_by_order[nums_count[number] - 1].add(number) # it adds number based on frequency's ascending order through indices
+        non_empty_buckets = [ bucket for bucket in buckets_by_order if bucket] # only none empty buckets to work on
+        flattened_list = []
+        for bucket in non_empty_buckets: # flattens the nested list but the order remains still
             for number in bucket:
-                final_list.append(number)
-                if len(final_list) == k:
-                    return final_list
-
+                flattened_list.append(number)
+        final_list = [] # final answer
+        unique_frequency = 0
+        frequency = 0
+        for number in reversed(flattened_list):
+            final_list.append(number)
+            if nums_count[number] != frequency: # it cares about only number with unique frequency
+                frequency = nums_count[number]
+                unique_frequency += 1
+            if unique_frequency == k:# stops when k number of unique frequency numbers are found
+                break
         return final_list
+a =  Solution()
+a.topKFrequent([1,2,2,4,4,6,7,7,7],3)
